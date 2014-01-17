4e0736d1cbed838e76034b94d0c0d4c13e6a7bcd
Damiano Braga <damiano.braga@gmail.com>
2014-01-16 20:35:40 -0800

    added plain empty node server

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+    app.configure(function(){
+    app.use('/media', express.static(__dirname + '/media'));
+    app.use('/js', express.static(__dirname + '/js'));
+    app.use('/css', express.static(__dirname + '/css'));
+    });
+
+server.listen(3000);
